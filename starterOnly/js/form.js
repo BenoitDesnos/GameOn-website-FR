631bea748ac9082d337d9d4faa99bc36a63250ba
Desnos <benoit.desnos6@gmail.com>
2023-01-17 02:16:42 +0100

    globalchecker ok, manque à integrer les checkboxes

--- starterOnly/js/form.js
+++ starterOnly/js/form.js
@@ -3,16 +3,14 @@
 const form = document.querySelector("form");
 const modalBody = document.querySelector(".modal-body");
 const formChildrens = document.querySelectorAll("form > *");
-const inputs = document.querySelectorAll("input");
+const inputs = document.querySelectorAll(
+  ".text-control" /* ~ S.checkbox-input" */
+);
+const errors = document.querySelectorAll(".error");
 const checkBoxes = document.querySelectorAll('input[type="radio"]');
 const usersTerms = document.getElementById("checkbox1");
 const quantity = document.getElementById("quantity");
 
-let isFirstEmpty = true;
-let isLastEmpty = true;
-let isEmailEmpty = true;
-let isBirthEmpty = true;
-let isQuantitySelected = false;
 let isOneCityChecked = false;
 let isUsersTermsAgreed = usersTerms.checked;
 
@@ -26,73 +24,41 @@ let regexEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
 
 /* Les fn ci-dessous verifient les values rentrées et retournent s'il y a une erreur ou non grâce à la fn errorDisplay*/
 
-// nameChecker est utilisée pour les champs prénom et nom
-const nameChecker = (value, id) => {
-  if (value.length < 2 || value.length > 60) {
+const valueChecker = (input, regex, checked) => {
+  let value = input.value;
+  let id = input.id;
+  /*  if (!checked) {
     errorDisplay(
       id,
-      "Le Prénom/Nom doit faire entre 2 et 60 caractères",
+      document.querySelector(`#${id}`).dataset.errornotchecked,
       false
     );
-    if (id === "first") {
-      isFirstEmpty = true;
-    } else {
-      isLastEmpty = true;
-    }
-  } else if (!value.match(regexText)) {
+  } else { */
+  if (value.length === 0) {
     errorDisplay(
       id,
-      "Le Prénom/Nom ne doit pas contenir de caractères spéciaux",
+      document.querySelector(`#${id}`).dataset.errorempty,
       false
     );
+    console.log("test1");
+  } else if (regex && !value.match(regex)) {
+    console.log("test2");
+    errorDisplay(
+      id,
+      document.querySelector(`#${id}`).dataset.errorregex,
+      false
+    );
+  } else if ((value.length < 2 || value.length > 60) && isNaN(value)) {
+    errorDisplay(
+      id,
+      document.querySelector(`#${id}`).dataset.errorlength,
+      false
+    );
+    console.log("test3");
   } else {
     errorDisplay(id, "", true);
-    if (id === "first") {
-      isFirstEmpty = false;
-    } else {
-      isLastEmpty = false;
-    }
   }
-};
-
-const emailChecker = (value, id) => {
-  if (!value.match(regexEmail)) {
-    errorDisplay(id, "Le mail n'est pas valide", false);
-    isEmailEmpty = true;
-  } else {
-    errorDisplay(id, "", true);
-    isEmailEmpty = false;
-  }
-};
-
-const birthChecker = (value, id) => {
-  if (value !== "") {
-    errorDisplay(id, "", true);
-    isBirthEmpty = false;
-  } else {
-    errorDisplay(id, "Merci de choisir une date de naissance", false);
-    isBirthEmpty = true;
-  }
-};
-
-const quantityChecker = (value, id) => {
-  if (!value.match(regexQuantity)) {
-    errorDisplay("quantity", "Merci de choisir un chiffre ou un nombre", false);
-    isQuantitySelected = false;
-  } else {
-    errorDisplay(id, "", true);
-    isQuantitySelected = true;
-  }
-};
-
-// checkCgu verifie si l'input sur laquelle nous la jouons est coché ou non
-const checkCgu = (isChecked, id) => {
-  if (isChecked) {
-    errorDisplay(id, "", true);
-  } else {
-    errorDisplay(id, "Merci d'accepter les CGU", false);
-    isQuantitySelected = false;
-  }
+  /*  } */
 };
 
 // fn gérant l'affichage des erreurs sur le DOM pour chaque fn de vérification
@@ -145,36 +111,40 @@ const closeThanksMessageOnClick = () => {
 
 // verifie chaque champs à chaque inputs effectués grace aux fn déclarées plus tot
 // les checkboxes radio ne sont pas vérifiées ici, elles sont vérifiées dans le forEach dessous
+function listenToAction(input) {
+  switch (input.attributes.type.value) {
+    case "text":
+      valueChecker(input, regexText);
+      break;
+    case "email":
+      valueChecker(input, regexEmail);
+      break;
+    case "number":
+      valueChecker(input, regexQuantity);
+      break;
+    /*  case "checkbox":
+      valueChecker(input, null, isUsersTermsAgreed);
+      break;
+    case "radio":
+      valueChecker(input, null, isOneCityChecked);
+      break; */
+
+    default:
+      valueChecker(input, null);
+
+      null;
+  }
+}
+
 inputs.forEach((input) => {
   input.addEventListener("input", (e) => {
-    switch (e.target.id) {
-      case "first":
-        nameChecker(e.target.value, e.target.id);
-        break;
-      case "last":
-        nameChecker(e.target.value, e.target.id);
-        break;
-      case "email":
-        emailChecker(e.target.value, e.target.id);
-        break;
-      case "birthdate":
-        birthChecker(e.target.value, e.target.id);
-        break;
-      case "checkbox1":
-        checkCgu(e.target.value, e.target.id);
-        break;
-      case "quantity":
-        quantityChecker(e.target.value, e.target.id);
-
-        break;
-
-      default:
-        null;
-    }
+    listenToAction(input);
   });
 });
+
 // si aucune villes n'est cochées alors passe isOneCityChecked sur true
 // pas de else car impossible de décocher
+
 checkBoxes.forEach((checkBox) => {
   checkBox.addEventListener("input", () => {
     if (!isOneCityChecked) {
@@ -187,48 +157,32 @@ checkBoxes.forEach((checkBox) => {
 //toggle les CGU sur true ou false
 usersTerms.addEventListener("click", () => {
   isUsersTermsAgreed = !isUsersTermsAgreed;
+  if (isUsersTermsAgreed) {
+    errorDisplay("checkbox1", "", true);
+  }
 });
 
 // verifie les inputs au moment de la submission
 form.addEventListener("submit", (e) => {
-  if (isFirstEmpty) {
-    errorDisplay("first", "Merci de renseigner votre prénom", false);
-    e.preventDefault();
-  }
-  if (isLastEmpty) {
-    errorDisplay("last", "Merci de renseigner votre nom", false);
-    e.preventDefault();
-  }
-  if (isEmailEmpty) {
-    errorDisplay("email", "Merci de renseigner votre email", false);
-    e.preventDefault();
-  }
-  if (isBirthEmpty) {
-    errorDisplay("birthdate", "Merci de choisir une date de naissance", false);
-    e.preventDefault();
-  }
-  if (!isQuantitySelected) {
-    errorDisplay("quantity", "Merci de choisir un nombre", false);
-    e.preventDefault();
-  }
+  e.preventDefault();
+  let isErrorPresent = false;
   if (!isOneCityChecked) {
-    e.preventDefault();
-    errorDisplay("radioContainer", "Merci de choisir une ville", false);
+    errorDisplay("radioContainer", "prout", false);
   }
   if (!isUsersTermsAgreed) {
-    errorDisplay("checkbox1", "Merci d'accepter les CGU", false);
-    e.preventDefault();
+    errorDisplay("checkbox1", "prout2", false);
   }
-  if (
-    !isFirstEmpty &&
-    !isLastEmpty &&
-    !isEmailEmpty &&
-    !isBirthEmpty &&
-    isQuantitySelected &&
-    isOneCityChecked &&
-    isUsersTermsAgreed
-  ) {
-    e.preventDefault();
+  inputs.forEach((input) => {
+    listenToAction(input);
+  });
+  errors.forEach((error) => {
+    console.log(error.textContent);
+    if (error.textContent.length > 0) {
+      isErrorPresent = true;
+    }
+  });
+  console.log(isErrorPresent);
+  if (!isErrorPresent) {
     displayThanksMessage();
     closeThanksMessageOnClick();
   }
